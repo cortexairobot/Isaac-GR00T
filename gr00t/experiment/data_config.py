e5825a118687d40dc7fe5f1c8dcf9ffe59ad9332
lucasngoo <lucasngoo@gmail.com>
2025-07-12 11:31:29 +0800

    Edit action_indices

--- gr00t/experiment/data_config.py
+++ gr00t/experiment/data_config.py
@@ -138,12 +138,12 @@ class FourierGr1ArmsOnlyDataConfig(BaseDataConfig):
 
 
 class So100DataConfig(BaseDataConfig):
-    video_keys = ["video.webcam"]
+    video_keys = ["video.front"]
     state_keys = ["state.single_arm", "state.gripper"]
     action_keys = ["action.single_arm", "action.gripper"]
     language_keys = ["annotation.human.task_description"]
     observation_indices = [0]
-    action_indices = list(range(16))
+    action_indices = list(range(50))
 
     def modality_config(self) -> dict[str, ModalityConfig]:
         video_modality = ModalityConfig(
